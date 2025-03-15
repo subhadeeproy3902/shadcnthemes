804dcfca18df686ae89e9ddadbecc9ec349edc74
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-15 21:19:30 +0530

    feat: add Badge component for enhanced UI elements and update Home page layout

---
+++ src/components/Badge.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const Badge = ({ text }: { text: string }) => {
+  return (
+    <div
+      className="bg-background/50 no-underline group cursor-pointer relative  shadow-2xl shadow-zinc-900 rounded-full p-px text-sm font-semibold leading-6 inline-block"
+    >
+      <span className="absolute inset-0 overflow-hidden rounded-full">
+        <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+      </span>
+      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-secondary/50 py-0.5 px-5 ring-1 ring-white/10 ">
+        <span>{text}</span>
+      </div>
+      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+    </div>
+  );
+};
+
+
+
